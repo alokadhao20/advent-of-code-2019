7222baecb0b1f8f8b565cf1829d68af469ba1ad6
Alok Adhao <alokadhao@gmail.com>
2020-02-29 00:52:50 +0800

    Update part-1.js
    Updated changes

--- day-6/part-1.js
+++ day-6/part-1.js
@@ -1,8 +1,14 @@
-const starMap = input.split('\n').reduce((map, line) => {
-  map[line.split(')')[1]] = line.split(')')[0];
+const starMap = input.split('\n').reduce((map, data) => {
+  map[data.split(')')[1]] = data.split(')')[0];
   return map;
 }, {});
 
-const getAncestorCount = body => body in starMap ? 1 + getAncestorCount(starMap[body]) : 0;
+const getAncestorCount = body => {
+  body in starMap ? 1 + getAncestorCount(starMap[body]) : 0
+};
 
-console.log(Object.keys(starMap).reduce((sum, body) => sum + getAncestorCount(body), 0));
+let result = Object.keys(starMap).reduce((sum, body) => {
+  sum + getAncestorCount(body), 0);
+}
+
+console.log("Final result is - ",result);
